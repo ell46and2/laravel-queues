4102a95f2b89d2259753da2455a1b552e0449a09
Elliot Alderton <ellalderton@gmail.com>
2018-03-22 23:08:12 +0000

    First commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Jobs\Users\UpdateLastLogin;
+use Carbon\Carbon;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+
+    // function comes from the AuthenticatesUsers trait
+    // It is called everytime a user successfully logs in
+    protected function authenticated(Request $request, $user)
+    {
+        $this->dispatch(new UpdateLastLogin($user, Carbon::now()));
+    }
+}
